461f35a5947299e75f8decea28f0abcd64c520a8
willshepp28 <willsheppard28@outlook.com>
2017-12-26 19:35:00 -0500

    initial commit

---
+++ types.ts
@@ -0,0 +1,31 @@
+// Variable Types
+
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any; // any data type
+let aVoid: void;
+let aNullType: null;
+let isUndefined: undefined;
+
+let strArr : string[]; // this has to be a array of strings
+let numArr: number[]; // this has to be a array of numbers
+let boolArr: boolean[]; // this has to be a array of booleans
+
+let strArr2: Array<string>; // this is another way to set a string of arrays
+let numArr2: Array<number>;
+let boolArr2: Array<boolean>; 
+
+let strNumTuple: [string, number]; // a array that has to be a sting and a number. Has to match exactly
+
+
+myString: "Life is good";
+myNum = 25;
+myBool = true;
+
+strArr = ['love', 'life', 'yolo'];
+numArr = [1,2,3,4];
+boolArr = [true, false, true];
+aVoid = null;
+
+console.log(myBool);
